19d3f637aa3755bc788e82eb1e2006ca896c5d0d
nhavilah <51215537+nhavilah@users.noreply.github.com>
2020-06-13 14:03:51 +1000

    Final Stocks Website
    Minor issue with NPM modules refusing to reinstall but can confirm it does work

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { LandingPageContent } from "./LandingPage.js";
+
+export function Home() {
+    return (
+        <div className="column-right1">
+            <LandingPageContent />
+        </div>
+    );
+}
